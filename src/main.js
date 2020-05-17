0a5cb21b39ba08a0a8654c616f35656b54799450
Lawrence Almeida <lawrence.almeida@unbabel.com>
2020-05-17 17:59:19 +0100

    Mount app on element

--- src/main.js
+++ src/main.js
@@ -6,13 +6,24 @@ import App from './App.vue';
 
 Vue.config.productionTip = false;
 
+const containerSelector = '#appOne-placeholder'
+
 const vueLifecycles = singleSpaVue({
   Vue,
   appOptions: {
     render: (h) => h(App),
+    el: containerSelector
   },
 });
 
 export const bootstrap = vueLifecycles.bootstrap;
 export const mount = vueLifecycles.mount;
 export const unmount = vueLifecycles.unmount;
+
+export const devtools = {
+  overlays: {
+    selectors: [
+      containerSelector
+    ],
+  }
+};
